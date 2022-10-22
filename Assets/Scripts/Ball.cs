48e78e2a1eb6fce0a90f9b6cc03435d6bd9a8799
zerefgd <solankikshitij1998@gmail.com>
2022-10-22 16:39:06 +0530

    Final Movements

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField]
-    private float distance, time;
+
+    [SerializeField] private float _distance, _time;
+
+    [SerializeField] private AnimationCurve _jumpingCurve, _fallingCurve;
 
     [SerializeField]
     private GameObject explosionPrefab;
 
-    private float speed, startSpeed, acceleration;
 
+
+    private float timeElapsed;
+    private bool isMovingTop;
+
+    private Vector3 startPos, endPos;
+    private Vector3 currentStartPos, currentEndPos, currentOffset;
+ 
     public bool hasGameStarted;
 
     private void Start()
     {
         hasGameStarted = false;
 
-        startSpeed = 2 * distance / time;
-        acceleration = -0.995f * startSpeed / time;
-        speed = startSpeed;
+        timeElapsed = 0f;
+        
+        startPos = transform.localPosition;
+        endPos = startPos + _distance * Vector3.up;        
+        isMovingTop = true;
+
+        currentStartPos = startPos;
+        currentEndPos = endPos;
+        currentOffset = currentEndPos - currentStartPos;
     }
 
     private void FixedUpdate()
@@ -28,31 +43,44 @@ public class Ball : MonoBehaviour
 
         if (!hasGameStarted) return;
 
-        speed += acceleration * Time.fixedDeltaTime;
-        Vector3 temp = new Vector3(0, speed * Time.fixedDeltaTime, 0);
-        transform.localPosition += temp;
-        temp = transform.localPosition;
+        timeElapsed += Time.fixedDeltaTime;
 
-        if(temp.y < 0f)
+        float offsetValue = timeElapsed / _time;
+        offsetValue = isMovingTop ? _jumpingCurve.Evaluate(offsetValue) : _fallingCurve.Evaluate(offsetValue);
+
+        transform.localPosition = currentStartPos +  offsetValue * currentOffset ;
+
+        if(timeElapsed >= _time)
         {
+            timeElapsed -= _time;
 
-            if(!Physics.Raycast(transform.position,Vector3.down,4f))
+            currentStartPos = isMovingTop ? endPos : startPos;
+            currentEndPos = isMovingTop ? startPos : endPos;
+
+            if (!isMovingTop)
             {
-                GameManager.instance.GameOver();
-                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-                Destroy(transform.parent.gameObject);
-            }
-            else
-            {
-                RaycastHit hit;
-                Physics.Raycast(transform.position, Vector3.down, out hit, 4f);
-                GameObject tempBlock = hit.transform.gameObject;
-                GameManager.instance.UpdateScore(tempBlock);
+
+                if (!Physics.Raycast(transform.position, Vector3.down, 4f))
+                {
+                    GameManager.instance.GameOver();
+                    Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                    Destroy(transform.parent.gameObject);
+                }
+                else
+                {
+                    RaycastHit hit;
+                    Physics.Raycast(transform.position, Vector3.down, out hit, 4f);
+                    GameObject tempBlock = hit.transform.gameObject;
+                    GameManager.instance.UpdateScore(tempBlock);
+                }
+
+                GameManager.instance.SpawnBlock();
             }
 
-            speed = startSpeed;
-            GameManager.instance.SpawnBlock();
-        }
+            isMovingTop = !isMovingTop;
+            currentOffset = currentEndPos - currentStartPos;
+        }        
+       
     }
 
     private void OnTriggerEnter(Collider other)
